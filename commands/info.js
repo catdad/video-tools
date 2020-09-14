59535fe4cd1a6eea1c0ba1cb707e46d818d851c6
Kiril Vatev <vatev.1@gmail.com>
2020-09-14 19:21:55 -0400

    fixing info command

--- commands/info.js
+++ commands/info.js
@@ -2,14 +2,8 @@ const path = require('path');
 
 const { ffprobe } = require('../lib/ffmpeg.js');
 
-async function handler(argv) {
-  const infile = path.resolve(argv.input);
-
-  await ffprobe(`"${infile}"`);
-}
-
 module.exports = {
   command: 'info <input>',
   describe: 'show ffprobe info',
-  handler
+  handler: async ({ input }) => await ffprobe(`"${path.resolve(input)}"`)
 };
