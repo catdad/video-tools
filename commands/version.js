bf819a712c729d65fbcd05c0fd4355e568558a73
Kiril Vatev <vatev.1@gmail.com>
2025-01-08 01:17:13 -0500

    adding version command

---
+++ commands/version.js
@@ -0,0 +1,30 @@
+const pkg = require('../package.json');
+const { ffmpeg, ffprobe } = require('../lib/ffmpeg.js');
+
+const getVersion = str => str.trim().split('\n')[0].trim();
+
+module.exports = {
+  command: 'version',
+  describe: 'show ffmpeg and ffprobe versions',
+  builder: (yargs) => yargs.option('silent', {
+    type: 'boolean',
+    describe: 'execute command without printing to console',
+    default: false
+  }),
+  handler: async ({ silent }) => {
+    const { stdout: ffprobeOut } = await ffprobe('-version', { stdout: null, stderr: null });
+    const { stdout: ffmpegOut } = await ffmpeg('-version', { stdout: null, stderr: null });
+
+    const libVersion = `${pkg.name} version ${pkg.version} Copyright (c) ${pkg.author}`;
+    const ffmpegVersion = getVersion(ffmpegOut);
+    const ffprobeVersion = getVersion(ffprobeOut);
+
+    if (!silent) {
+      console.log(libVersion);
+      console.log(ffmpegVersion);
+      console.log(ffprobeVersion);
+    }
+
+    return { libVersion, ffmpegVersion, ffprobeVersion };
+  }
+};
